e694dde3aec1ab4ed5995403a86045e7b5d8d10c
Phidias Mendez <mendezphidias@gmail.com>
2020-03-26 23:33:23 -0400

    Encoding Function

--- huffman.py
+++ huffman.py
@@ -3,22 +3,50 @@ import sys
 import marshal
 import array
 import heapq
+import collections
 
 try:
     import cPickle as pickle
 except:
     import pickle
 
-def encode(msg):
+def createRing(htree):
+    heapq.heapify(htree)
+    while len(htree) > 1:
+        left = heapq.heappop(htree)
+        right = heapq.heappop(htree)
+        for sumNode in left[1:]:
+            sumNode[1] = '0' + sumNode[1]
+        
+        for sumNode in right[1:]:
+            sumNode[1] = '1' + sumNode[1]
 
-    raise NotImplementedError
+        heapq.heappush(htree, [left[0] + right[0]] + left[1:] + right[1:])
+    codes = sorted(heapq.heappop(htree)[1:], key=lambda p: (len(p[-1]), p))
+    ring = dict()
+
+    for code in codes:
+        ring[code[0]] = code[1]
+
+    return ring
+
+def encode(msg):
+    print("Original MSG:", msg)
+    count = collections.Counter(msg)
+    htree = [[weight, [char, '']] for char, weight in count.items()]
+    ring = createRing(htree)
+    cipher = str()
+
+    for char in msg:
+        cipher += ring[char]
+
+    return cipher, ring
 
 def decode(msg, decoderRing):
-
+    
     raise NotImplementedError
 
 def compress(msg):
-
     # Initializes an array to hold the compressed message.
     compressed = array.array('B')
     raise NotImplementedError
@@ -67,7 +95,6 @@ if __name__=='__main__':
             fcompressed.close()
         else:
             enc, decoder = encode(msg)
-            print(msg)
             fcompressed = open(outfile, 'wb')
             marshal.dump((pickle.dumps(decoder), enc), fcompressed)
             fcompressed.close()
